0234ca70fd3f73b09963c1e98c14362cb5a670b4
C00282704 <144837556+C00282704@users.noreply.github.com>
2023-09-28 13:28:11 +0100

    Update lab2bq1.java

--- Lab2b/lab2bq1/lab2bq1.java
+++ Lab2b/lab2bq1/lab2bq1.java
@@ -3,11 +3,11 @@ public class lab2bq1
 public static void main(String args []) 
 	{ 
 		
-		Rectangle RecA = new Rectangle();
-		RecA.setlength(7);
-		RecA.setwidth(34);
+		Rectangle recA = new Rectangle();
+		recA.setlength(7);
+		recA.setwidth(34);
 		
-		System.out.println(RecA);
+		System.out.println(recA);
 		
 	}
 }
