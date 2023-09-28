f0a5f59c047ee4978e5d04b508b3ca77bb9f5029
C00282704 <144837556+C00282704@users.noreply.github.com>
2023-09-28 13:27:15 +0100

    Update lab2bq1.java

--- Lab2b/lab2bq1/lab2bq1.java
+++ Lab2b/lab2bq1/lab2bq1.java
@@ -3,7 +3,7 @@ public class lab2bq1
 public static void main(String args []) 
 	{ 
 		
-		Dimensions RecA = new Dimensions();
+		Rectangle RecA = new Rectangle();
 		RecA.setlength(7);
 		RecA.setwidth(34);
 		
